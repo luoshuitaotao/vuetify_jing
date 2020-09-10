cc088315c8f79a6d1e4c36338ad458013ed7063a
luoshuitaotao <jcui@unomaha.edu>
2020-09-09 23:12:44 -0500

    add buttons and icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <p class="red white--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto doloremque nostrum sunt minima id hic error voluptatibus neque expedita? Debitis necessitatibus suscipit quae laudantium possimus minus nesciunt iusto voluptatem similique.</p>
-    <p class="pink lighten-4 red--text cyan lighten-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, magni at explicabo nisi ex possimus rerum dolore perspiciatis placeat eos esse voluptas corrupti repellat porro error cupiditate vitae laudantium soluta.</p>
-    <h1 class="display-4">Massive Display</h1>
-    <h4 class="display-1">Small display</h4>
-    <p class="headline">This is a headline</p>
-    <p class= "subheading text-decoration-underline">This is a subheading</p>
-    <p class="caption">This is a caption</p>
-  </div>
+    <v-btn class="pink white--text">Click me</v-btn>
+    <v-btn depressed color="pink">Click me</v-btn>
+    <v-btn rounded color="pink">Click ME</v-btn>
+    <v-btn flat color="pink">Click ME</v-btn>
+
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn depressed  small class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>Email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+        <span>Email me</span>
+        <v-icon right>mdi-email</v-icon>
+    </v-btn>
+
+    <v-btn depressed small fab dark color="purple">
+        <v-icon>mdi-heart</v-icon>
+    </v-btn>
+    </div>
 </template>
 
 <script>
@@ -17,4 +34,3 @@
 export default {
 }
 </script>
-s
