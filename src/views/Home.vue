3dfbb2883858e6bf7e27c294509cc8f6102aa327
luoshuitaotao <jcui@unomaha.edu>
2020-09-10 15:49:33 -0500

    add breakpoints visibility

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,9 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-    <v-btn class="pink white--text">Click me</v-btn>
-    <v-btn depressed color="pink">Click me</v-btn>
-    <v-btn rounded color="pink">Click ME</v-btn>
-    <v-btn flat color="pink">Click ME</v-btn>
-
-
-    <v-btn depressed class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>Email me</span>
-    </v-btn>
-
-    <v-btn depressed  small class="pink white--text">
-      <v-icon left>mdi-email</v-icon>
-      <span>Email me</span>
-    </v-btn>
-
-    <v-btn depressed large class="pink white--text">
-        <span>Email me</span>
-        <v-icon right>mdi-email</v-icon>
-    </v-btn>
-
-    <v-btn depressed small fab dark color="purple">
-        <v-icon>mdi-heart</v-icon>
-    </v-btn>
+    <!--<v-btn class= "hidden-md-and-down">click me</v-btn>-->
+    <!--<v-btn class="hidden-md-and-up">Click me</v-btn>-->
+    <v-btn class="hidden-sm-only">Click me</v-btn>
     </div>
 </template>
 
