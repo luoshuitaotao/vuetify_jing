36afe11a92bea866d6892e408c71456a8ab11935
luoshuitaotao <jcui@unomaha.edu>
2020-09-09 22:36:24 -0500

    add colors and text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+    <p class="red white--text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto doloremque nostrum sunt minima id hic error voluptatibus neque expedita? Debitis necessitatibus suscipit quae laudantium possimus minus nesciunt iusto voluptatem similique.</p>
+    <p class="pink lighten-4 red--text cyan lighten-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, magni at explicabo nisi ex possimus rerum dolore perspiciatis placeat eos esse voluptas corrupti repellat porro error cupiditate vitae laudantium soluta.</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Small display</h4>
+    <p class="headline">This is a headline</p>
+    <p class= "subheading text-decoration-underline">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
+s
