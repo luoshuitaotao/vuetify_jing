74fbee8dfd486d97d23f5fac818d838201983625
luoshuitaotao <jcui@unomaha.edu>
2020-09-13 16:08:51 -0500

    add lists

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,16 @@
         </v-toolbar>
 
         <v-navigation-drawer v-model="drawer" app class="primary">
-            <p class="error">test</p>
+            <v-list dark>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-tile-action>
+                        <v-icon>{{ link.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-tile-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
     </nav>
 </template>
@@ -22,7 +31,12 @@
 export default {
     data(){
         return{
-            drawer:false
+            drawer:false,
+            links:[
+              { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+              { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+              { icon: 'mdi-account', text: 'Team', route: '/team' },
+            ]
         }
     }
     
