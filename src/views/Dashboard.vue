6e33ca99af483941b45d8d648064b0519fab1ac0
luoshuitaotao <jcui@unomaha.edu>
2020-09-13 22:30:54 -0500

    use chips

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -18,8 +18,9 @@
             <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs2 sm4 md2>
-            <div class="caption grey--text">Status</div>
-            <div>{{ project.status }}</div>
+            <div class="text-end">
+              <v-chip small :color="`${project.status}`" :class="`v-chip--active white--text caption my-2`">{{ project.status }}</v-chip>
+            </div>
           </v-flex>
         </v-layout>
         <v-divider></v-divider>
@@ -54,5 +55,15 @@ export default {
 .project.overdue {
   border-left: 4px solid tomato;
 }
+.v-chip.complete{
+  background: #3cd1c2;
+}
+.v-chip.ongoing{
+  background: #ffaa2c;
+}
+.v-chip.overdue{
+  background: #f83e70;
+}
+
 </style>
 
