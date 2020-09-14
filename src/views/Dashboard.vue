cde99013f0681a78ebaae4343d7f32d2d9e45db5
luoshuitaotao <jcui@unomaha.edu>
2020-09-13 21:17:18 -0500

    add web grid on dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,29 +2,26 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
+      <v-card flat class="pa-3">
         <v-layout row wrap>
-          <v-flex xs12 md6>
-            <v-btn outline block class="primary">1</v-btn>
+          <v-flex xs="12" md="6">
+            <div class="caption grey--text">Project title</div>
+            <div>Create a new website</div>
           </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary">2</v-btn>
+          <v-flex xs="6" sm="4" md="2">
+            <div class="caption grey--text">Person</div>
+            <div>The Net Jing</div>
           </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary">2</v-btn>
+          <v-flex xs="6" sm="4" md="2">
+            <div class="caption grey--text">Due by</div>
+            <div>1st Jan 2020</div>
           </v-flex>
-          <v-flex xs4 md2>
-            <v-btn outline block class="primary">2</v-btn>
-          </v-flex>          
-        </v-layout>
-
-        <v-layout row wrap justify-space-around>
-          <v-flex xs4 md3>
-            <v-btn outline block class="success">1</v-btn>
-          </v-flex>
-          <v-flex xs4 md3>
-            <v-btn outline block class="success">2</v-btn>
+          <v-flex xs="2" sm="4" md="2">
+            <div class="caption grey--text">Status</div>
+            <div>ongoing</div>
           </v-flex>
         </v-layout>
+      </v-card>
     </v-container>
   </div>
 </template>
