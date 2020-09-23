c63b7dea5ea944ee0d52e63e83ab0c87df8c68c7
luoshuitaotao <jcui@unomaha.edu>
2020-09-23 16:25:44 -0500

    add dropdown button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,6 +7,19 @@
                 <span>Jing</span>
             </v-toolbar-title>
             <v-spacer></v-spacer>
+            <v-menu offset-y>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn flat color="grey" dark v-bind="attrs" v-on="on">
+                    <span>MENU</span>
+                    </v-btn>
+                </template>
+                <v-list>
+                    <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item-title>{{ link.text }}</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+
             <v-btn flat color="grey">
                 <span>Sign out</span>
                 <v-icon right>mdi-exit-to-app</v-icon>
